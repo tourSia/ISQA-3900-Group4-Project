d2910995012e0c239ad23425ba55c0759150422c
tourSia <124312754+tourSia@users.noreply.github.com>
2023-05-18 22:42:50 -0500

    Update

--- GroceryStore/settings.py
+++ GroceryStore/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-kxrqi^+u-mc4rs9kxfug5d0j#o^b6ejbom)teh406r7@+@j#f#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
 
 # Application definition
